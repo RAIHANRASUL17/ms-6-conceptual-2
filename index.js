aaa084e610fd94ca7928340d7bb1925a531a423c
RAIHANRASUL17 <smrihanrasul17cqut@gmail.com>
2023-02-25 21:52:03 +0800

    all country and fligs with click

---
+++ index.js
@@ -0,0 +1,13 @@
+const person={
+    name: 'Raihan Rasul',
+    job: 'no job',
+    student: true,
+    university: 'Yangzhou Uninversity',
+    course: 'ms- electrical',
+    roll: 21055,
+    American: false,
+    country: 'BDG'
+}
+
+const jsonStringified= JSON.stringify(person);
+console.log(jsonStringified)
